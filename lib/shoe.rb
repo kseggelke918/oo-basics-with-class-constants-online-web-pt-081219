6e1dbc82b5d239db5f713b008743c70d84fbac3c
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-28 16:58:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -6,6 +7,10 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    brand.each do |shoe|
+      if shoe 
+        binding.pry 
+      end 
     BRANDS << brand 
   end
 

9d861d67fd987fe3a9da2958ea0c2b793b444551
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-28 17:10:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-      BRANDS << brand unless BRANDS.include? (shoe[index])
+      BRANDS << brand unless BRANDS.include? (brand)
   end
   
 

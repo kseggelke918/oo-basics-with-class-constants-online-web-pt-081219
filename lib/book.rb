a27839abc559fb2ff5155efa04cfdfcc09a7b051
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-28 16:45:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,6 +7,14 @@ class Book
   def initialize(title)
     @title = title
   end
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"

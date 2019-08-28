54fb6dcb0e1ea2948dbea475fe86aecb471c8cde
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-28 17:09:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,10 +7,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    brand.each_with_index do |shoe, index|
       BRANDS << brand unless BRANDS.include? (shoe[index])
-    end 
-    # BRANDS << brand  
   end
   
 
